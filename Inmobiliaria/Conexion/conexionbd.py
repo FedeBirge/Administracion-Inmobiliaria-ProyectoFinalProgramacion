d6eb5eb53fec1203241d3cb4e9db823ee7fee0b4
Luciana Ferreyra <mar.lu.ferreyra@gmail.com>
2022-11-01 23:18:59 -0300

    actualizo clase conexion

--- Inmobiliaria/Conexion/conexionbd.py
+++ Inmobiliaria/Conexion/conexionbd.py
@@ -1,17 +1,52 @@
 import mysql.connector
-try:
-    conexion = mysql.connector.connect(
-        host='localhost',
-        port=3306,
-        user='root',
-        password='Feddu#1985',
-        db='Inmobiliaria'
-    )
-   
-except:
-    print('Error al conectar a la Base de Datos')
+import time
+
+class conexion: # Clase conexion
+    def __init__(self):
+        try:
+            self.conexion = mysql.connector.connect(
+                host='localhost',
+                port=3306,
+                user='root',
+                password='Feddu#1985',
+                db='Inmobiliaria'
+            )
+        except mysql.connector.Error as err:
+            print('Error al conectar a la Base de Datos')
+            print(err)
+
+    def getConection(self):
+        return self.conexion
+
+    def setConection(self, conexion):
+        self.conexion = conexion
+
+    def __str__(self):
+        return "Conexion a la Base de Datos"
+
+    def conectar(self): # Metodo para conectar la base de datos
+        try:
+            self.conexion.connect()
+            if self.conexion.is_connected():
+                print('Conexión exitosa ' +
+                      time.strftime("%x") + '  ' + time.strftime("%X"))
+        except mysql.connector.Error as err:
+            print('Error al conectar a la Base de Datos')
+            print(err)
+
+    def desconectar(self): # Metodo para desconectar la base de datos   
+        try:
+            if self.conexion.is_connected():
+                self.conexion.close()
+                print('Conexión cerrada ' +
+                      time.strftime("%x") + '  ' + time.strftime("%X"))
+        except mysql.connector.Error as err:
+            print('Error al conectar a la Base de Datos')
+            print(err)
+
+    def conectado(self): # Metodo para verificar si la conexion esta activa
+        return self.conexion.is_connected()
+
 
 if __name__ == '__main__':
-    print('Soy el modulo de conexión a la Base de Datos')
-    
-        
+    print('Soy la clase de conexión a la Base de Datos')
